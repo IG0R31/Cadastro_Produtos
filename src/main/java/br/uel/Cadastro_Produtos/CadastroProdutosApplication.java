242f8c6dfea0114ca1fd0f56ce1e3152e4d82a79
IG0R31 <igor.oliveiraduarte05@gmail.com>
2025-09-29 16:09:49 -0300

    INICIANDO PROJETO DE CADASTRO_CONTATOS

---
+++ src/main/java/br/uel/Cadastro_Produtos/CadastroProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.uel.Cadastro_Produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uel/Cadastro_Produtos/CadastroProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uel.Cadastro_Produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
